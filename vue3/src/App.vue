b7e716aad7e06aaefe35b9d89d397857d1277151
ismail <issmail.essafi@gmail.com>
2021-09-02 14:00:06 +0100

    try v-for v-bind

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,19 +1,24 @@
 <template>
-  <div v-html="title"></div>
-  <p v-if="isVueJs">bonjour dans Vue.js</p>
-  <p v-else-if="isVueJs === null">bonjour dans Angular</p>
-  <p v-else>bonjour dans React</p>
+  <div>{{title}}</div>
+  <ul>
+    <li v-for="names in freindz" v-bind:key="names">{{names.name}}</li>
+    <input v-bind:value="hero">
+  </ul>
 </template>
 
 <script>
 export default ({
   data() {
     return{
-      title: "<h1>hi world !</h1>",
-      isVueJs: false,
+      title: "hi world !",
+      freindz: [
+        {name :'waldi'},
+        {name :'mostapha'},
+        {name :'redwan'}
+    ],
+      hero:"superman"
     }
     
   }
 })
 </script>
-
