7b2d73d1125b2c0491991539bc9d846e0988b959
ismail <issmail.essafi@gmail.com>
2021-09-08 14:05:07 +0100

    add css to app and try components

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,65 +1,13 @@
 <template>
-  <div>{{title}} {{fullname}}</div>
-  <p>{{randC}}</p>
-  <p>{{randC}}</p>
-  <p>{{randC}}</p><br>
-  <p>{{randM()}}</p>
-  <p>{{randM()}}</p>
-  <p>{{randM()}}</p>
 
-  <ul>
-    <li v-for="names in freindz" v-bind:key="names">{{names.name}}</li>
-    <input v-model="freind">
-    <button v-on:click="addName">add nom</button><br>
-    <button v-on:click="edit_fullname">Edit number_of_freindz</button>
-  </ul>
+  <NameList />
+
 </template>
 
 <script>
-export default ({
+import NameList from "./components/NameList";
 
-  computed:{
-      fullname:{
-        get(){ 
-          return `${this.fname} ${this.lname}`;
-        },
-        set(fullname){
-          const values=fullname.split(" ");
-          this.fname=values[0];
-          this.lname=values[1];
-        }
-      },
-      randC(){
-        return Math.random() ;
-      },
-
-  },
-
-  methods: {
-    addName(){
-      this.freindz.push({name:this.freind});
-      this.freind=''
-    },
-    randM(){
-      return Math.random();
-    },
-    edit_fullname(){
-      this.fullname="walid fisawi";
-    }
-  },
-  data() {
-    return{
-      title: "hi world  ",
-      freindz: [
-        {name :'waldi'},
-        {name :'mostapha'},
-        {name :'redwan'}
-    ],
-      freind:"",
-      fname: "ismail",
-      lname: "essafi"
-    }
-    
-  }
-})
+export default {
+ components: { NameList },
+};
 </script>
---
+++ vue3/src/components/NameList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+  <div class="title-list">{{title}} {{name_Number}}</div>
+
+  <ul>
+    <li v-for="(names,index) in freindz" v-bind:key="names.name">
+      <span>{{names.name}}</span><button v-on:click="remove_name(index)">x</button>
+      </li>
+    <input v-model="freind">
+    <button v-on:click="addName">add nom</button><br>
+  </ul>
+</div>
+</template>
+
+<script>
+  export default {
+    computed: {
+        name_Number() {
+        return this.freindz.length;
+        },
+    },
+    methods: {
+        addName(){
+        this.freindz.push({name:this.freind});
+        this.freind=''
+        },
+        remove_name(index){
+        this.freindz=this.freindz.filter((names,i)=> i !== index);
+        }
+    },
+    data() {
+        return{
+        title: "hi world  ",
+        freindz: [
+            {name :'waldi'},
+            {name :'mostapha'},
+            {name :'redwan'}
+        ],
+        freind:"",
+        }
+        
+    }
+  };
+</script>
