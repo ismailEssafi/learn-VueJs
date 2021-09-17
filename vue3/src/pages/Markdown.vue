2178de3d82c60645f44497be08d55e0577a2cbaa
ismail <issmail.essafi@gmail.com>
2021-09-17 22:17:27 +0100

    create Markdown app

--- vue3/src/pages/Markdown.vue
+++ vue3/src/pages/Markdown.vue
@@ -1,10 +1,37 @@
 <template>
-  Markdown
+  <div class="markdown">
+    <h1>Markdown App</h1>
+    <section class="input-output">
+      <article class="input-text">
+        <textarea v-on:input="update" v-bind:value="text"></textarea>
+      </article>
+      <article class="output-text" v-html="markedText">
+      </article>            
+    </section>
+  </div>  
 </template>
 
 <script>
+import marked from 'marked' ;
+import debounce from '../utilities/mixins/debounce';
 export default {
-
+  mixins :[debounce],
+  data(){
+    return {
+      text : "left",
+    }
+  },
+  computed: {
+      markedText(){
+          return marked(this.text);
+      }, 
+  },
+  methods: {
+      update(e){ 
+        const task=() => {this.text=e.target.value}; 
+        this.debounce(task);   
+      },
+  }
 }
 </script>
 
