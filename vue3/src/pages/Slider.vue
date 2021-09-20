f2925c721ef82206107551ea712491e13458e823
ismail <issmail.essafi@gmail.com>
2021-09-20 17:20:29 +0100

    finishing Slider app with control pointer

--- vue3/src/pages/Slider.vue
+++ vue3/src/pages/Slider.vue
@@ -1,22 +1,29 @@
 <template>
-    <div v-if="countdown == 1" class="slide1"></div>
-    <div v-if="countdown == 2" class="slide2"></div>
-    <div v-if="countdown == 3" class="slide3"></div>
+    <div v-for="(style,index) in styles" :key="index">
+        <transition name="fade">
+            <div v-if="countdown == index" class="slide" :style="style"></div>
+        </transition>       
+    </div>
+    <div class="indicators">
+        <div v-for="(style,index) in styles" :style="countdown==index ? 'background-color:#2C3E50':'background-color:white'" v-on:click="countdown=index" class="indicator" :key="index"></div>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return{
-            countdown:1,
-            interval:""
+            countdown:0,
+            interval:"",
+            isShowing:false,
+            styles: ["background-color:teal","background-color:tomato","background-color:yellow"]
         }
     },
     mounted(){
         this.interval=setInterval(()=>{
             console.log(this.countdown);
-            if(this.countdown==3){
-                this.countdown=1
+            if(this.countdown==2){
+                this.countdown=0
             }else{
                 this.countdown++;
             }     
@@ -30,5 +37,17 @@ export default {
 </script>
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: all 1s ease;
+}
 
+.fade-enter-from{
+    opacity: 0;
+    transform:translateX(-100%)
+}
+.fade-leave-to {
+    opacity: 0;
+    transform:translateX(100%)
+}
 </style>
