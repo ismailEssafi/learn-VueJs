2601167eb8cdaa51bd60e18c748fa03a678aebd4
ismail <issmail.essafi@gmail.com>
2021-10-05 10:49:03 +0100

    Calculator app complete

--- vue3/src/pages/Calculator.vue
+++ vue3/src/pages/Calculator.vue
@@ -1,35 +1,90 @@
 <template>
-    <section class="calulator">   
+    <section class="calculator">   
         <h1>Calculator</h1>
-        <p type="number">{{number}}</p>
+        <p>{{currentNum}}</p>
+        <small v-if="selectedOperation">{{prevNum}}{{selectedOperation}}{{currentNum}}</small>
         <div class="cal-buttons">
-            <button @click="addNumber(1)">1</button>
-            <button @click="addNumber(2)">2</button>
-            <button @click="addNumber(3)">3</button>
-            <button @click="addNumber(4)">4</button>
-            <button @click="addNumber(5)">5</button>   
-            <button @click="addNumber(6)">6</button>
-            <button @click="addNumber(7)">7</button>
-            <button @click="addNumber(8)">8</button>
-            <button @click="addNumber(9)">9</button>
-            <button @click="addNumber(0)">0</button>
-            <button>=</button>
+            <button @click="pressed('1')">1</button>
+            <button @click="pressed('2')">2</button>
+            <button @click="pressed('3')">3</button>
+            <button @click="pressed('+')">+</button>
+            <button @click="pressed('4')">4</button>
+            <button @click="pressed('5')">5</button>   
+            <button @click="pressed('6')">6</button>
+            <button @click="pressed('-')">-</button>
+            <button @click="pressed('7')">7</button>
+            <button @click="pressed('8')">8</button>
+            <button @click="pressed('9')">9</button>
+            <button @click="pressed('*')">*</button>
+            <button @click="pressed('0')">0</button>
+            <button @click="pressed('c')">C</button>
+            <button @click="pressed('=')">=</button>
+            <button @click="pressed('/')">/</button>
+
         </div>    
     </section>    
 </template>
 
 <script>
-import {ref} from 'vue';
+import {onMounted, onUnmounted, ref} from 'vue';
 export default {
     setup(){
-        const inputNum=ref([]);
+        const currentNum=ref("");
         const number=ref("");
-        function addNumber(num){
-            inputNum.value.push(num);
-            number.value=+inputNum.value.join("");
-
+        const operator=['+','-','*','/'];
+        const prevNum=ref("");
+        const selectedOperation=ref("");
+        const onlynumber=['1','2','3','4','5','6','7','8','9','0'];
+        function pressed(value){
+            if(value === '=' || value === "Enter") calculate();
+            else if(operator.includes(value)) applyOperation(value);
+            else if(value === 'c') clear();
+            else if (onlynumber.includes(value)) appendNumber(value)
+            
         }
-        return {inputNum,number,addNumber}
+        function appendNumber(value){
+            currentNum.value=currentNum.value + value ;
+        }
+
+        function calculate(){
+            if (selectedOperation.value === '+') sum()
+            else if (selectedOperation.value === '-') subtract()
+            else if (selectedOperation.value === '*') multiply()
+            else if (selectedOperation.value === '/') divide();
+
+            prevNum.value="";
+            selectedOperation.value=""
+        }
+
+        function applyOperation(value){
+            calculate();
+            prevNum.value=currentNum.value;
+            currentNum.value="";
+            selectedOperation.value=value;
+        }
+
+        function clear(){
+            currentNum.value="";
+        }
+        function sum(){
+            currentNum.value=+prevNum.value + +currentNum.value
+        }
+        function subtract(){
+            currentNum.value=+prevNum.value - +currentNum.value
+        }
+        function multiply(){
+            currentNum.value=+prevNum.value * +currentNum.value
+        }
+        function divide(){
+            currentNum.value=+prevNum.value / +currentNum.value
+        }
+        function keydownHandler(e){
+            pressed(e.key);
+        }
+        onMounted(()=>window.addEventListener("keydown", keydownHandler));
+        onUnmounted(()=>window.removeEventListener("keydown", keydownHandler));
+
+        return {currentNum,number,pressed,prevNum,selectedOperation}
     }
 
 }
