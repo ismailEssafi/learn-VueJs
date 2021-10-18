9a70988714dd93db70cdb21d73a7e1613cae7668
ismail <issmail.essafi@gmail.com>
2021-10-18 14:51:31 +0100

    chat app with firebase is complete

--- vue3/src/pages/Chat.vue
+++ vue3/src/pages/Chat.vue
@@ -1,26 +1,52 @@
 <template>
-    <div v-for="(chat,index) in state.chats" :key="index">{{chat.message}}</div>
+    <div class="chats">
+        <h1>Chat App</h1>
+        <div v-for="(chat,index) in state.chats" :key="index" 
+        :class="chat.userId == state.userId ? '' : 'otherUser'">{{chat.message}}</div>
+    </div>
+    <input 
+    class="message" 
+    type="text" 
+    placeholder="Start Typing..." 
+    v-model="state.message"
+    @keydown.enter="addMessage">
 </template>
 
 <script>
-import { getDatabase, ref,onValue} from "firebase/database";
+import { getDatabase, ref,onValue,set,push } from "firebase/database";
+import { getAuth } from "firebase/auth";
 import { onMounted,reactive } from '@vue/runtime-core';
 
 export default {
 setup(){
     const state=reactive({
-        chats:{}
+        chats:{},
+        message:"",
+        userId : null 
     });
     onMounted(()=>{
         const db = getDatabase();
         const refrence = ref(db, 'chat/');
         onValue(refrence, (snapshot) => {
         state.chats = snapshot.val();
-        
-});
+        });
+
+        const auth = getAuth();
+        state.userId=auth.currentUser.uid;
     })
 
-    return{state}
+    function addMessage(){
+        const db = getDatabase();
+        const refrence = ref(db, 'chat/');
+        const newMessagetRef = push(refrence);
+        set(newMessagetRef, {
+            message : state.message,
+            userId :state.userId
+        });
+        state.message=""
+    }
+
+    return{state,addMessage}
 }
 }
 </script>
